9d5b708cd214a8f2c8c0c095c0386171604c5879
Kevin Cianfarini <kevincianfarini@gmail.com>
2022-05-10 17:39:52 -0400

    Dump output data to SQL files (#2)

---
+++ src/nativeTest/kotlin/org/climatechangemakers/parsecongress/DumpToSqlTest.kt
@@ -0,0 +1,149 @@
+package org.climatechangemakers.parsecongress
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class DumpToSqlTest {
+
+  @Test fun `dumping a single member of congress produces expected result`() {
+    val members = listOf(
+      ClimateChangemakersMemberOfCongress(
+        bioguideId = "C000001",
+        fullName = "Kevin Cianfarini",
+        legislativeRole = "rep",
+        state = "VA",
+        congressionalDistrict = 4,
+        party = "Democrat",
+        dcPhoneNumber = "555.555.5555",
+        twitterHandle = null,
+        cwcOfficeCode = null,
+      )
+    )
+
+    assertEquals(
+      expected = """
+        |INSERT INTO member_of_congress(bioguide_id, full_name, legislative_role, state, congressional_district, party, dc_phone_number, twitter_handle, cwc_office_code)
+        |VALUES
+        |('C000001', 'Kevin Cianfarini', 'rep', 'VA', 4, 'Democrat', '555.555.5555', NULL, NULL)
+        |ON CONFLICT(bioguide_id) DO UPDATE
+        |SET (bioguide_id, full_name, legislative_role, state, congressional_district, party, dc_phone_number, twitter_handle, cwc_office_code) = (EXCLUDED.bioguide_id, EXCLUDED.full_name, EXCLUDED.legislative_role, EXCLUDED.state, EXCLUDED.congressional_district, EXCLUDED.party, EXCLUDED.dc_phone_number, EXCLUDED.twitter_handle, EXCLUDED.cwc_office_code);
+      """.trimMargin(),
+      actual = dumpToSql(members)
+    )
+  }
+
+  @Test fun `dumping a single member of congress escapes apostrophe`() {
+    val members = listOf(
+      ClimateChangemakersMemberOfCongress(
+        bioguideId = "C000001",
+        fullName = "Kevin Ci'anfarini",
+        legislativeRole = "rep",
+        state = "VA",
+        congressionalDistrict = 4,
+        party = "Democrat",
+        dcPhoneNumber = "555.555.5555",
+        twitterHandle = null,
+        cwcOfficeCode = null,
+      )
+    )
+
+    assertEquals(
+      expected = """
+        |INSERT INTO member_of_congress(bioguide_id, full_name, legislative_role, state, congressional_district, party, dc_phone_number, twitter_handle, cwc_office_code)
+        |VALUES
+        |('C000001', 'Kevin Ci''anfarini', 'rep', 'VA', 4, 'Democrat', '555.555.5555', NULL, NULL)
+        |ON CONFLICT(bioguide_id) DO UPDATE
+        |SET (bioguide_id, full_name, legislative_role, state, congressional_district, party, dc_phone_number, twitter_handle, cwc_office_code) = (EXCLUDED.bioguide_id, EXCLUDED.full_name, EXCLUDED.legislative_role, EXCLUDED.state, EXCLUDED.congressional_district, EXCLUDED.party, EXCLUDED.dc_phone_number, EXCLUDED.twitter_handle, EXCLUDED.cwc_office_code);
+      """.trimMargin(),
+      actual = dumpToSql(members)
+    )
+  }
+
+  @Test fun `dumping multiple members of congress produces the expected result`() {
+    val members = listOf(
+      ClimateChangemakersMemberOfCongress(
+        bioguideId = "C000001",
+        fullName = "Kevin Cianfarini",
+        legislativeRole = "rep",
+        state = "VA",
+        congressionalDistrict = 4,
+        party = "Democrat",
+        dcPhoneNumber = "555.555.5555",
+        twitterHandle = null,
+        cwcOfficeCode = null,
+      ),
+      ClimateChangemakersMemberOfCongress(
+        bioguideId = "C000002",
+        fullName = "Kevin Cianfarini 2",
+        legislativeRole = "sen",
+        state = "VA",
+        congressionalDistrict = null,
+        party = "Republican",
+        dcPhoneNumber = "555.555.5556",
+        twitterHandle = null,
+        cwcOfficeCode = null,
+      )
+    )
+
+    assertEquals(
+      expected = """
+        |INSERT INTO member_of_congress(bioguide_id, full_name, legislative_role, state, congressional_district, party, dc_phone_number, twitter_handle, cwc_office_code)
+        |VALUES
+        |('C000001', 'Kevin Cianfarini', 'rep', 'VA', 4, 'Democrat', '555.555.5555', NULL, NULL),
+        |('C000002', 'Kevin Cianfarini 2', 'sen', 'VA', NULL, 'Republican', '555.555.5556', NULL, NULL)
+        |ON CONFLICT(bioguide_id) DO UPDATE
+        |SET (bioguide_id, full_name, legislative_role, state, congressional_district, party, dc_phone_number, twitter_handle, cwc_office_code) = (EXCLUDED.bioguide_id, EXCLUDED.full_name, EXCLUDED.legislative_role, EXCLUDED.state, EXCLUDED.congressional_district, EXCLUDED.party, EXCLUDED.dc_phone_number, EXCLUDED.twitter_handle, EXCLUDED.cwc_office_code);
+      """.trimMargin(),
+      actual = dumpToSql(members)
+    )
+  }
+
+  @Test fun `dumping a single district office produces expected result`() {
+    val offices = listOf(
+      ClimateChangemakersDistrictOffice(
+        bioguide = "C000002",
+        phoneNumber = "804.555.3456",
+        lat = 1.0,
+        long = 2.0,
+      ),
+    )
+
+    assertEquals(
+      expected = """
+        |TRUNCATE district_office;
+        |INSERT INTO district_office(bioguide_id, phone_number, lat, long)
+        |VALUES
+        |('C000002', '804.555.3456', 1.0, 2.0);
+      """.trimMargin(),
+      actual = dumpToSql(offices),
+    )
+  }
+
+  @Test fun `dumping multiple distrcit offices produces expected result`() {
+    val offices = listOf(
+      ClimateChangemakersDistrictOffice(
+        bioguide = "C000002",
+        phoneNumber = "804.555.3456",
+        lat = 1.0,
+        long = 2.0,
+      ),
+      ClimateChangemakersDistrictOffice(
+        bioguide = "C000002",
+        phoneNumber = "804.556.3456",
+        lat = null,
+        long = null,
+      ),
+    )
+
+    assertEquals(
+      expected = """
+        |TRUNCATE district_office;
+        |INSERT INTO district_office(bioguide_id, phone_number, lat, long)
+        |VALUES
+        |('C000002', '804.555.3456', 1.0, 2.0),
+        |('C000002', '804.556.3456', NULL, NULL);
+      """.trimMargin(),
+      actual = dumpToSql(offices),
+    )
+  }
+}
